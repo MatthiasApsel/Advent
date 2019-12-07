41eb7805434e564ecd0d4568783380df5af8f354
Matthias Apsel <matthias.apsel@selfhtml.org>
2019-12-07 14:04:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+$data = [
+  '1' => [
+    'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
+    'heading' => 'SVG skalieren',
+    'author' => 'Gunnar Bittersmann am 13.11.2019',
+    'teaser' => 'Ich hab mal ein Beispiel gebaut, in welchem dasselbe Symbol zweimal in unterschiedlichen Größen verwendet wird.',
+  ],
+  '2' => [
+    'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
+    'heading' => 'Feststehender Footer',
+    'author' => 'Gunnar Bittersmann am 27.3.2018',
+    'teaser' => 'Wenn Header und Footer ständig im Viewport bleiben sollen, dann ist position:&nbsp;sticky das Mittel der Wahl.',
+  ],
+  '3' => [ 
+    'href' => 'https://forum.selfhtml.org/self/2019/jun/22/mathematik-zur-veroffentlichung-von-version-5-der-forumssoftware/1750933#m1750933',
+    'heading' => 'Quintupelspielerereien',
+    'author' => 'Matthias Apsel am 22.6.2019',
+    'teaser' => 'Aus Anlass der Veröffentlichung V5 unseres Forums eine mathematische Spielerei.',
+  ],
+  '4' => [
+    'href' => 'https://forum.selfhtml.org/self/2019/jun/5/speisekarte-mit-punkte-zwischen-speisen-und-punkt-punkt-punkt-preis/1750026#m1750026',
+    'heading' => 'Speisekarte mit Punkten',
+    'author' => 'Gunnar Bittersmann am 05.06.2019',
+    'teaser' => 'Eine Beschreibungsliste (description list, dl), deren Elemente jeweils links und rechts zentriert und durch Punkte verbunden sind.',
+  ],
+  '5' => [
+    'href' => 'https://forum.selfhtml.org/self/2018/jan/7roblem-mit-css-grid/1711672#m1711672',
+    'heading' => 'Ausklappbare Navigation',
+    'author' => 'Gunnar Bittersmann am 09.01.2018',
+    'teaser' => 'Eine Navigation (nav ul), die auf kleinen Viewports hinter einem Button verschwindet.',
+  ],
+  '6' => [
+    'href' => 'https://forum.selfhtml.org/self/2018/sep/15/text-anordnen/1732000#m1732000',
+    'heading' => 'Header links neben Text',
+    'author' => 'Gunnar Bittersmann am 15.09.2018',
+    'teaser' => 'Ein Header mit einem teaser, der mit Grid links neben dem Haupttext angeordnet wird.',
+  ],
+  '7' => [
+    'href' => 'https://forum.selfhtml.org/self/2018/jul/26/frage-zum-wiki-artikel-border/1727371#m1727371',
+    'heading' => 'Überschrift mit Rand',
+    'author' => 'Gunnar Bittersmann am 26.07.2018',
+    'teaser' => 'Eine Überschrift, deren Rand nur so lang wie der Inhalt ist &rarr; geht mit <code>max-width: max-content</code>',
+  ],
+];
+
+$default = 'Dieses Türchen öffnet sich erst am $$date$$. Dezember.';
+$donate = [
+  'href' => 'https://selfhtml.org/spenden.html',
+  'text' => 'Sie können das Projekt SELFHTML unterstützen.',
+];
+
+
+echo '<pre>';
+var_dump($data);
+echo '</pre>';
+?>
