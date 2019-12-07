773908cf3c60145ec9b6e6e6083d6384ee5b91c8
Matthias Apsel <matthias.apsel@selfhtml.org>
2019-12-07 17:15:50 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,175 +1,38 @@
 <?php
+require ('data.php');
+
 $title = 'Adventskalender 2019';
-$data = [
-  '1' => [
-    'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
-    'heading' => 'SVG skalieren',
-    'author' => 'Gunnar Bittersmann am 13.11.2019',
-    'teaser' => 'Ich hab mal ein Beispiel gebaut, in welchem dasselbe Symbol zweimal in unterschiedlichen Größen verwendet wird.',
-  ],
-  '2' => [
-    'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
-    'heading' => 'Feststehender Footer',
-    'author' => 'Gunnar Bittersmann am 27.3.2018',
-    'teaser' => 'Wenn Header und Footer ständig im Viewport bleiben sollen, dann ist position:&nbsp;sticky das Mittel der Wahl.',
-  ],
-  '3' => [ 
-    'href' => 'https://forum.selfhtml.org/self/2019/jun/22/mathematik-zur-veroffentlichung-von-version-5-der-forumssoftware/1750933#m1750933',
-    'heading' => 'Quintupelspielerereien',
-    'author' => 'Matthias Apsel am 22.6.2019',
-    'teaser' => 'Aus Anlass der Veröffentlichung V5 unseres Forums eine mathematische Spielerei.',
-  ],
-  '4' => [
-    'href' => 'https://forum.selfhtml.org/self/2019/jun/5/speisekarte-mit-punkte-zwischen-speisen-und-punkt-punkt-punkt-preis/1750026#m1750026',
-    'heading' => 'Speisekarte mit Punkten',
-    'author' => 'Gunnar Bittersmann am 05.06.2019',
-    'teaser' => 'Eine Beschreibungsliste (description list, dl), deren Elemente jeweils links und rechts zentriert und durch Punkte verbunden sind.',
-  ],
-  '5' => [
-    'href' => 'https://forum.selfhtml.org/self/2018/jan/7roblem-mit-css-grid/1711672#m1711672',
-    'heading' => 'Ausklappbare Navigation',
-    'author' => 'Gunnar Bittersmann am 09.01.2018',
-    'teaser' => 'Eine Navigation (nav ul), die auf kleinen Viewports hinter einem Button verschwindet.',
-  ],
-  '6' => [
-    'href' => 'https://forum.selfhtml.org/self/2018/sep/15/text-anordnen/1732000#m1732000',
-    'heading' => 'Header links neben Text',
-    'author' => 'Gunnar Bittersmann am 15.09.2018',
-    'teaser' => 'Ein Header mit einem teaser, der mit Grid links neben dem Haupttext angeordnet wird.',
-  ],
-  '7' => [
-    'href' => 'https://forum.selfhtml.org/self/2018/jul/26/frage-zum-wiki-artikel-border/1727371#m1727371',
-    'heading' => 'Überschrift mit Rand',
-    'author' => 'Gunnar Bittersmann am 26.07.2018',
-    'teaser' => 'Eine Überschrift, deren Rand nur so lang wie der Inhalt ist &rarr; geht mit <code>max-width: max-content</code>',
-  ],
-  '8' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '9' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '10' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '11' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '12' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '13' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '14' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '15' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '16' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '17' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '18' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '19' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '20' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '21' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '22' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '23' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-  '24' => [
-    'href' => 'https://example.com',
-    'heading' => 'Überschrift',
-    'author' => 'Autor',
-    'teaser' => 'Teaser',
-  ],
-];
-$default = 'Dieses Türchen öffnet sich erst am $$date$$. Dezember.';
-$donate = [
-  'href' => 'https://selfhtml.org/spenden.html',
-  'text' => 'Sie können das Projekt SELFHTML unterstützen.',
-];
 
 $now = time();
 $today = date("j");
 $listitems = [];
 for ($k = 1; $k < sizeof($data); $k++) :
   $allowed = mktime(0,0,0,12,$k,2019);
-  if ($now < $allowed) :
-    $listitems[] = '<p>'.str_replace('$$date$$',$k,$default).'</p>';
-  else :
+  if ($now > $allowed && $k <= $today) :
     $listitems[] = '
       <a href="'.$data[$k]['href'].'">
         <h2>'.$data[$k]['heading'].'</h2>
         <p>'.$data[$k]['author'].'</p>
         <p>'.$data[$k]['teaser'].'</p>
       </a>';
+  elseif ($k == $today + 1) :
+    $listitems[] = '<a href="'.$donate['href'].'">'.$donate['text'].'</a>';
+  else :
+    $listitems[] = '<p>'.str_replace('$$date$$',$k,$default).'</p>';
   endif;
 endfor;
 
-?>
+$allowed = mktime(0,0,0,12,24,2019);
+if ($now < $allowed) :
+  $listitems[] = '<a href="'.$donate['href'].'">'.$donate['text'].'</a>';
+else :
+  $listitems[] = '
+    <a href="'.$data[24]['href'].'">
+      <h2>'.$data[24]['heading'].'</h2>
+      <p>'.$data[24]['author'].'</p>
+      <p>'.$data[24]['teaser'].'</p>
+    </a>';
+endif; ?>
 
 <!DOCTYPE html>
 <html lang="de">
@@ -177,6 +40,7 @@ endfor;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
+    <style><?php include('css.php') ?></style>
   </head>
   <body>
     <h1><?=$title?></h1>
