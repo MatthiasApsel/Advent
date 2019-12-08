499303a6b7c67ca94c30d4558afe9b4c84c66957
Matthias Apsel <matthias.apsel@selfhtml.org>
2019-12-08 14:09:14 +0100

    numeric keys

--- data.php
+++ data.php
@@ -1,144 +1,144 @@
 <?php
 $data = [
-  '1' => [
+  1 => [
     'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
     'heading' => 'SVG skalieren',
     'author' => 'Gunnar Bittersmann am 13.11.2019',
     'teaser' => 'Ich hab mal ein Beispiel gebaut, in welchem dasselbe Symbol zweimal in unterschiedlichen Größen verwendet wird.',
   ],
-  '2' => [
+  2 => [
     'href' => 'https://forum.selfhtml.org/self/2018/mar/27/footer-immer-ganz-unten/1717543#m17175430',
     'heading' => 'Feststehender Footer',
     'author' => 'Gunnar Bittersmann am 27.3.2018',
     'teaser' => 'Wenn Header und Footer ständig im Viewport bleiben sollen, dann ist position:&nbsp;sticky das Mittel der Wahl.',
   ],
-  '3' => [ 
+  3 => [ 
     'href' => 'https://forum.selfhtml.org/self/2019/jun/22/mathematik-zur-veroffentlichung-von-version-5-der-forumssoftware/1750933#m1750933',
     'heading' => 'Quintupelspielerereien',
     'author' => 'Matthias Apsel am 22.6.2019',
     'teaser' => 'Aus Anlass der Veröffentlichung V5 unseres Forums eine mathematische Spielerei.',
   ],
-  '4' => [
+  4 => [
     'href' => 'https://forum.selfhtml.org/self/2019/jun/5/speisekarte-mit-punkte-zwischen-speisen-und-punkt-punkt-punkt-preis/1750026#m1750026',
     'heading' => 'Speisekarte mit Punkten',
     'author' => 'Gunnar Bittersmann am 05.06.2019',
     'teaser' => 'Eine Beschreibungsliste (description list, dl), deren Elemente jeweils links und rechts zentriert und durch Punkte verbunden sind.',
   ],
-  '5' => [
+  5 => [
     'href' => 'https://forum.selfhtml.org/self/2018/jan/7roblem-mit-css-grid/1711672#m1711672',
     'heading' => 'Ausklappbare Navigation',
     'author' => 'Gunnar Bittersmann am 09.01.2018',
     'teaser' => 'Eine Navigation (nav ul), die auf kleinen Viewports hinter einem Button verschwindet.',
   ],
-  '6' => [
+  6 => [
     'href' => 'https://forum.selfhtml.org/self/2018/sep/15/text-anordnen/1732000#m1732000',
     'heading' => 'Header links neben Text',
     'author' => 'Gunnar Bittersmann am 15.09.2018',
     'teaser' => 'Ein Header mit einem teaser, der mit Grid links neben dem Haupttext angeordnet wird.',
   ],
-  '7' => [
+  7 => [
     'href' => 'https://forum.selfhtml.org/self/2018/jul/26/frage-zum-wiki-artikel-border/1727371#m1727371',
     'heading' => 'Überschrift mit Rand',
     'author' => 'Gunnar Bittersmann am 26.07.2018',
     'teaser' => 'Eine Überschrift, deren Rand nur so lang wie der Inhalt ist &rarr; geht mit <code>max-width: max-content</code>',
   ],
-  '8' => [
+  8 => [
     'href' => 'https://forum.selfhtml.org/self/2019/feb/8/mathematik-zum-wochenende/1742252#m1742252',
     'heading' => 'Geraden in der Ebene',
     'author' => 'Matthias Apsel am 08.02.2018',
     'teaser' => 'In wieviele Gebiete kann eine Ebene durch <em>n</em> Geraden geteilt werden?',
   ],
-  '9' => [
+  9 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '10' => [
+  10 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '11' => [
+  11 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '12' => [
+  12 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '13' => [
+  13 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '14' => [
+  14 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '15' => [
+  15 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '16' => [
+  16 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '17' => [
+  17 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '18' => [
+  18 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '19' => [
+  19 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '20' => [
+  20 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '21' => [
+  21 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '22' => [
+  22 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '23' => [
+  23 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
     'teaser' => 'Teaser',
   ],
-  '24' => [
+  24 => [
     'href' => 'https://example.com',
     'heading' => 'Überschrift',
     'author' => 'Autor',
